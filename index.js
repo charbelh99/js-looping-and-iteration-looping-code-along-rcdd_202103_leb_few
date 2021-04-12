ce33b0248314c654d0f04070e722b538bfea7001
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-12 18:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+function writeCards(x,y){
+  const salamo = [];
+  for (let i = 0; i < x.length; i++) {
+    salamo.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`);
+  }
+  return salamo;
+  
+}
+
+ const writeCard = ['Lisa', 'Kaitlin', 'Jan'];
+ 
+ writeCards(writeCard,"surprise");
+
+ function countDown(x){
+   let count = x;
+   while (count>=0){
+     console.log(`${count}`);
+    // if(count === x){
+      // console.log()
+     //}
+     count--;
+     
+   }
+ }
+ countDown(10);
+ countDown(4);
+ 
